fc408aca9a776386b263f0d26e324fa46786935b
TingYu <h851206@gmail.com>
2022-02-27 20:50:10 +0100

    add TextExamples and TextdataPrep.py

--- Ger2eng.py
+++ Ger2eng.py
@@ -26,4 +26,76 @@ train_data, valid_data, test_data = Multi30k(language_pair=("de", "en"))
 german.build_vocab(train_data, max_size=10000, min_freq=2)
 english.build_vocab(train_data, max_size=10000, min_freq=2)
 
-print(type(train_data))
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        src_vocab_size,
+        trg_vocab_size,
+        embedding_size,
+        max_len,
+        num_heads,
+        num_encoder_layers,
+        num_decoder_layers,
+        forward_expansion,
+        dropout,
+        src_pad_idx
+    ) -> None:
+        super(Transformer, self).__init__()
+
+        self.src_word_embedding = nn.Embedding(src_vocab_size, embedding_size)
+        self.trg_word_embedding = nn.Embedding(trg_vocab_size, embedding_size)
+        self.src_pos_embedding = nn.Embedding(max_len, embedding_size)
+        self.trg_pos_embedding = nn.Embedding(max_len, embedding_size)
+
+        self.Transformer = nn.Transformer(
+            d_model=embedding_size,
+            nhead=num_heads,
+            num_encoder_layers=num_encoder_layers,
+            num_decoder_layers=num_decoder_layers,
+            dim_feedforward=forward_expansion,
+            dropout=dropout
+        )
+
+        self.fc_out = nn.Linear(embedding_size, trg_vocab_size)
+        self.dropout = nn.Dropout(dropout)
+        self.src_prd_idx = src_pad_idx
+
+    def make_src_mask(self, src):
+        # mask out the word which are padding
+        # src shape = (src_len, N)
+        src_mask =  src.transpose(0,1) == self.src_prd_idx
+        # the output is (N, src_len)
+        return src_mask
+
+    def forward(
+        self,
+        src,
+        trg
+    ):
+        src_seq_length, N = src.shape
+        trg_seq_length, N = trg.shape
+
+        # create position tensor which has the same size as src and trg
+        # (src_seq_length) -> (src_seq_length, 1) -> (src_seq_length, N)
+        src_position = torch.arange(0, src_seq_length).unsqueeze(1).expand(src_seq_length, N) 
+        trg_position = torch.arange(0, trg_seq_length).unsqueeze(1).expand(trg_seq_length, N)
+
+        embed_src = self.dropout(
+            self.src_word_embedding(src) + self.src_pos_embedding(src_position)
+        )
+        embed_trg = self.dropout(
+            self.trg_word_embedding(trg) + self.trg_pos_embedding(trg_position)
+        )
+        src_padding_mask = self.make_src_mask(src)
+        trg_mask = self.Transformer.generate_square_subsequent_mask(trg_seq_length)
+        
+        out = self.Transformer(
+            embed_src,
+            embed_trg,
+            src_key_padding_mask=src_padding_mask,
+            tgt_mask = trg_mask
+        )
+
+        out = self.fc_out(self.dropout(out))
+
+        return out
---
+++ TextdataPre.py
@@ -0,0 +1,81 @@
+# STEPS:
+
+# 1. Specify how preprocessing should be done -> Fields
+# 2. Use Dataset to load the data -> TabularDataset (JSON/CSV/TSV Files)
+# 3. Construct an iterator to do batching and padding -> BucketIterator
+
+from torchtext.legacy.data import Field, BucketIterator, TabularDataset
+import spacy
+import torch
+
+# specify tokenizer
+spacy_en = spacy.load("en_core_web_sm")
+def tokenize_en(text):
+    return [tok.text for tok in spacy_en.tokenizer(text)]
+# in this data set there are score and quote, but score is not text
+quote = Field(sequential=True, use_vocab=True, tokenize=tokenize_en, lower=True)
+score = Field(sequential=False, use_vocab=False)
+
+# specify what preprocessing function should be used in each column
+fields = {"quote":("q", quote), "score":("s", score)}
+# "quote" means the column name in the data. 
+# "q" means we can use batch.q to access the data after batches are created. 
+# Variable quote is specifying that the preprocessing function for that column
+
+# create the dataset for our own data
+train_data, test_data = TabularDataset.splits(
+    path="TextExample",
+    train="train.json",
+    test="test.json",
+    format= "json",
+    fields= fields
+)
+
+# # have an insight
+# print(train_data[0].__dict__.keys())
+# print(train_data[0].__dict__.values())
+
+# word embedding: tokenized word to integer
+quote.build_vocab(train_data, max_size=10000, min_freq=1)
+# quote.build_vocab(train_data, max_size=10000, min_freq=1, vectors='glove.6B.100d') #1GB
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# create the iterator
+train_iterator, test_iterator = BucketIterator.splits(
+    datasets=(train_data, test_data),
+    batch_size=2,
+    device=device
+)
+
+# for batch in train_iterator:
+#     print(batch.q)
+#     print(batch.s)
+
+
+# =============================================================================================
+# =============================================================================================
+
+from torchtext.legacy.datasets import Multi30k
+
+spacy_ger = spacy.load("de_core_news_sm")
+def tokenize_ger(text):
+    return [tok.text for tok in spacy_ger.tokenizer(text)]
+english = Field(sequential=True, use_vocab=True, lower=True, tokenize=tokenize_en)
+german = Field(sequential=True, use_vocab=True, lower=True, tokenize=tokenize_ger)
+
+train_data1, valid_data1, test_data1 = Multi30k.splits(exts=(".de", ".en"), fields=(german, english))
+
+german.build_vocab(train_data1, max_size=10000, min_freq=2)
+english.build_vocab(train_data1, max_size=10000, min_freq=2)
+
+train_iterator, validation_iterator, test_iterator = BucketIterator.splits(
+    (train_data1, valid_data1, test_data1),
+    batch_size=32,
+    device=device
+)
+# for batch in train_iterator:
+#     print(batch)
+print(english.vocab.stoi["the"])
+print(english.vocab.itos[5])
+# =============================================================================================
+# =============================================================================================
